325ee861d4aa936c2a7ab9bae86f6d848eb69ee1
shaistaakhtar <83580673+shaistaakhtar@users.noreply.github.com>
2021-09-26 15:36:09 +0530

    Add files via upload

---
+++ student details.c
@@ -0,0 +1,170 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+struct Student
+{
+    int rollnumber;
+    char name[30];
+    int age;
+    float marks;
+    struct Student *next;
+    
+}
+* head;
+void insert(int rollnumber, char* name, int* age, float marks)
+{
+    
+    struct Student * student = (struct Student *) malloc(sizeof(struct Student));
+    student->rollnumber = rollnumber;
+    strcpy(student->name, name);
+    strcpy(student->age, age);
+    student->marks = marks;
+    student->next = NULL;
+    
+    if(head==NULL){
+        // if head is NULL
+        // set student as the new head
+        head = student;
+    }
+    else{
+        // if list is not empty
+        // insert student in beginning of head
+        student->next = head;
+        head = student;
+    }
+    
+}
+void search(int rollnumber)
+{
+    struct Student * temp = head;
+    while(temp!=NULL){
+        if(temp->rollnumber==rollnumber){
+            printf("Roll Number: %d\n", temp->rollnumber);
+            printf("Name: %s\n", temp->name);
+            printf("age: %d\n", temp->age);
+            printf("marks: %0.4f\n", temp->marks);
+            return;
+        }
+        temp = temp->next;
+    }
+    printf("Student with roll number %d is not found !!!\n", rollnumber);
+}
+void update(int rollnumber)
+{
+    
+    struct Student * temp = head;
+    while(temp!=NULL){
+        
+        if(temp->rollnumber==rollnumber){
+            printf("Record with roll number %d Found !!!\n", rollnumber);
+            printf("Enter new name: ");
+            scanf("%s", temp->name);
+            printf("Enter new age: ");
+            scanf("%d", temp->age);
+            printf("Enter new marks: ");
+            scanf("%f",&temp->marks);
+            printf("Updation Successful!!!\n");
+            return;
+        }
+        temp = temp->next;
+        
+    }
+    printf("Student with roll number %d is not found !!!\n", rollnumber);
+    
+}
+void Delete(int rollnumber)
+{
+    struct Student * temp1 = head;
+    struct Student * temp2 = head; 
+    while(temp1!=NULL){
+        
+        if(temp1->rollnumber==rollnumber){
+            
+            printf("Record with roll number %d Found !!!\n", rollnumber);
+            
+            if(temp1==temp2){
+                // this condition will run if
+                // the record that we need to delete is the first node
+                // of the linked list
+                head = head->next;
+                free(temp1);
+            }
+            else{
+                // temp1 is the node we need to delete
+                // temp2 is the node previous to temp1
+                temp2->next = temp1->next;
+                free(temp1); 
+            }
+            
+            printf("Record Successfully Deleted !!!\n");
+            return;
+            
+        }
+        temp2 = temp1;
+        temp1 = temp1->next;
+        
+    }
+    printf("Student with roll number %d is not found !!!\n", rollnumber);
+    
+}
+void display()
+{
+    struct Student * temp = head;
+    while(temp!=NULL){
+        
+        printf("Roll Number: %d\n", temp->rollnumber);
+        printf("Name: %s\n", temp->name);
+        printf("Phone: %d\n", temp->age);
+        printf("marks: %0.4f\n\n", temp->marks);
+        temp = temp->next;
+        
+    }
+}
+int main()
+{
+    head = NULL;
+    int choice;
+    char name[30];
+    int age;
+    int rollnumber;
+    float marks;
+    printf("1 to insert student details\n2 to search for student details\n3 to delete student details\n4 to update student details\n5 to display all student details");
+    do
+    {
+        printf("\nEnter Choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1:
+                printf("Enter roll number: ");
+                scanf("%d", &rollnumber);
+                printf("Enter name: ");
+                scanf("%s", name);
+                printf("Enter age: ");
+                scanf("%s", age);
+                printf("Enter marks: ");
+                scanf("%f", &marks);
+                insert(rollnumber, name, age, marks);
+                break;
+            case 2:
+                printf("Enter roll number to search: ");
+                scanf("%d", &rollnumber);
+                search(rollnumber);
+                break;
+            case 3:
+                printf("Enter roll number to delete: ");
+                scanf("%d", &rollnumber);
+                Delete(rollnumber);
+                break;
+            case 4:
+                printf("Enter roll number to update: ");
+                scanf("%d", &rollnumber);
+                update(rollnumber);
+                break;
+            case 5:
+                display();
+                break;
+        }
+        
+    } while (choice != 0);
+}
